d8b81ba018b6cd721a453a6adddf0b3edf9b1c97
Ahmed Mahmoud <ahmeddahabmohamed@gmail.com>
2023-05-05 15:24:01 +0300

    test added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'test-angular-payment';
+export class AppComponent implements OnInit {
+  constructor() {}
+  handler: any = null;
+  ngOnInit() {
+    this.loadStripe();
+  }
+
+  pay(amount: any) {
+    var handler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_51JrEivIL1i6LlfhOcr9RvNaPA03yUt07KzxMJKht1FDD07v1AOXnGuKScbtIRjyEuyCt8Fe56O4DcLauoaFUSkl4002FdDgKXp',
+      locale: 'auto',
+      token: function (token: any) {
+        // You can access the token ID with `token.id`.
+        // Get the token ID to your server-side code for use.
+
+        console.log(token);
+        alert('Token Created!!');
+      },
+    });
+
+    handler.open({
+      name: 'Demo Site',
+      description: '2 widgets',
+      amount: amount * 100,
+    });
+  }
+
+  loadStripe() {
+    if (!window.document.getElementById('stripe-script')) {
+      var s = window.document.createElement('script');
+      s.id = 'stripe-script';
+      s.type = 'text/javascript';
+      s.src = 'https://checkout.stripe.com/checkout.js';
+      s.onload = () => {
+        this.handler = (<any>window).StripeCheckout.configure({
+          key: 'pk_test_51JrEivIL1i6LlfhOcr9RvNaPA03yUt07KzxMJKht1FDD07v1AOXnGuKScbtIRjyEuyCt8Fe56O4DcLauoaFUSkl4002FdDgKXp',
+          locale: 'auto',
+          token: function (token: any) {
+            // You can access the token ID with `token.id`.
+            // Get the token ID to your server-side code for use.
+            console.log(token);
+            alert('Payment Success!!');
+          },
+        });
+      };
+
+      window.document.body.appendChild(s);
+    }
+  }
 }
